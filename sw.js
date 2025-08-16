7d5388e0cd0c73be58a4d9dca92059aa784739c4
loofeht <89503199+loofehtt@users.noreply.github.com>
2025-08-16 23:20:22 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,13 +10,7 @@ self.addEventListener('install', event => {
   );
   self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
 });
